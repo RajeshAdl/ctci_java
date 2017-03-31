c240434f3e8e9e3563a12a06e23a87c099eff0e1
RajeshAdloori <rajesh@huskers.unl.edu>
2017-03-31 16:27:43 -0500

    Adding changes

--- chapter_1/Is_Unique.java
+++ chapter_1/Is_Unique.java
@@ -2,7 +2,7 @@ package chapter_1;
 
 public class Is_Unique {
 	
-	public static boolean is_unique(String s) {
+	public static boolean is_unique1(String s) {
 		if(s.length() > 128) {
 			return false;
 		}
@@ -18,10 +18,31 @@ public class Is_Unique {
 		return true;
 	}
 	
+	public static boolean is_unique2(String s) {
+		if(s.length() > 26) {
+			return false;
+		}
+		
+		int checker = 0;
+		for(int i=0; i<s.length(); i++) {
+			int val = s.charAt(i) - 'a';
+			if((checker & (1<<val)) > 0) {
+				return false;
+			}
+			checker = (1<<val) | checker;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		String[] input = {"abcde", "hello", "apple", "kite", "padle"};
+		System.out.println("Testing first function");
 		for(String in : input) {
-			System.out.println(in+ " " + is_unique(in));
+			System.out.println(in+ " " + is_unique1(in));
+		}
+		System.out.println("Testing second function");
+		for(String in : input) {
+			System.out.println(in+ " " + is_unique2(in));
 		}
 	}
 
