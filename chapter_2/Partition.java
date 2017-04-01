47429685844f3f55c22ea07c28c87181a80083a8
RajeshAdloori <rajesh@huskers.unl.edu>
2017-04-01 15:24:57 -0500

    Adding partition problem

---
+++ chapter_2/Partition.java
@@ -0,0 +1,73 @@
+package chapter_2;
+
+import library.LinkedListNode;
+
+public class Partition {
+
+	public static LinkedListNode partition(LinkedListNode node, int x) {
+		LinkedListNode head = node;
+		LinkedListNode tail = node;
+
+		while(node != null) {
+			LinkedListNode next = node.next;
+			if(node.data < x) {
+				node.next = head;
+				head = node;
+			}
+			else {
+				tail.next = node;
+				tail = node;
+			}
+			node = next;
+		}
+		tail.next = null;
+		return head;
+	}
+
+	public static LinkedListNode partition2(LinkedListNode node, int x) {
+		LinkedListNode afterStart = null;
+		LinkedListNode beforeStart = null;
+		LinkedListNode afterEnd = null;
+		LinkedListNode beforeEnd = null;
+
+		while(node != null) {
+			LinkedListNode next = node.next;
+			if(node.data < x) {
+				if(beforeStart == null) {
+					beforeStart = node;
+					beforeEnd = node;
+				}
+				else {
+					beforeEnd.next = node;
+					beforeEnd = node;
+				}
+			}
+			else {
+				if(afterStart == null) {
+					afterStart = node;
+					afterEnd = node;
+				}
+				else {
+					afterEnd.next = node;
+					afterEnd = node;
+				}
+			}
+			node = next;
+		}
+
+		if(beforeStart == null) {
+			return afterStart;
+		}
+		
+		beforeEnd.next = afterStart;
+		return beforeStart;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {1, 4, 6, 7, 3, 2, 5, 9};
+		LinkedListNode head = LinkedListNode.buildList(a);
+		LinkedListNode.printList(head);
+		head = partition2(head, 5);
+		LinkedListNode.printList(head);
+	}
+}
